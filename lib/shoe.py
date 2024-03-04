1b7b48d7ab0998b6d4fdc8c46e939810a62884b3
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-03-04 22:48:23 +0300

    completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = 'New'  # Added attribute 'condition' with default value 'New'
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = 'New'  # Set 'condition' to 'New' after repair
+
+    @property
+    def condition(self):
+        return self._condition
